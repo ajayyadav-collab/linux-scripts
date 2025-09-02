cee1f6b507d7a2996a22d143850e3fd64b54c9ff
root <root@linux2.us-central1-c.c.pro-runway-462217-j5.internal>
2025-09-02 07:58:37 +0000

    Added automated backup script

---
+++ backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Backup source directory
+SOURCE="/var/log"
+
+# Backup destination directory
+DEST="/home/ajay/backups"
+
+# Create destination directory if not exists
+mkdir -p $DEST
+
+# Filename with date
+FILENAME="backup-$(date +%Y-%m-%d_%H-%M-%S).tar.gz"
+
+# Create backup
+tar -czf $DEST/$FILENAME $SOURCE
+
+# Print message
+echo "Backup saved as $DEST/$FILENAME"
+
+# Delete backups older than 7 days
+find $DEST -type f -name "*.tar.gz" -mtime +7 -exec rm {} \;
+
+echo "Old backups (7+ days) deleted"
+
